844fda68de6bba9d7453cc0f3c8d45237f198cb9
Al Madireddy <al.madireddy@gmail.com>
2019-02-24 05:51:27 -0600

    build

---
+++ build/precache-manifest.c72fdd61f153fc7842df77e0a26050cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9c3ae6b52e619e94555",
+    "url": "/static/css/main.05b629ba.chunk.css"
+  },
+  {
+    "revision": "b9c3ae6b52e619e94555",
+    "url": "/static/js/main.b9c3ae6b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ee7217ba498a29d33cff",
+    "url": "/static/js/2.ee7217ba.chunk.js"
+  },
+  {
+    "revision": "5fb0cf6cdc8b40a352cf34c7cb2cfcc6",
+    "url": "/index.html"
+  }
+];
